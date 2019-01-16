e2c228e48dc2fcadc368e6a8c438ed925a197757
Alex <alex@dprk.dprk>
2019-01-16 16:17:20 +0000

    added log file naming by date and scale checking for multiple values

--- ls_prototype.c
+++ ls_prototype.c
@@ -21,6 +21,7 @@ compile using gcc as >gcc -W -Wall ls_prototype -o ls_prototype -lm
 #include "stdbool.h"
 #include "string.h"
 #include "stdlib.h"
+#include <time.h>
 
 
 
@@ -31,6 +32,8 @@ double _5ht;
 double sf;
 
 FILE* f; 
+
+
 
 
 double 
@@ -59,8 +62,8 @@ run(int sf, double scale, double _5ht, int r_delay ,int r_life)
 	int c1 = 0;
 	int c2 = 0;
 	
-	printf("\n\n\nRun with scaling factor %d, scale length %g, reward delay %d, reward lifetime %d\n\n\n",sf,scale,r_delay,r_life);
-	printf("__________________________________________________________________________\n\n\n\n");
+	//printf("\n\n\nRun with scaling factor %d, scale length %g, reward delay %d, reward lifetime %d\n\n\n",sf,scale,r_delay,r_life);
+	//printf("__________________________________________________________________________\n\n\n\n");
 
 	fprintf(f,"\n\n\nRun with scaling factor %d, scale length %g, reward delay %d, reward lifetime %d\n\n\n",sf,scale,r_delay,r_life);
 	fprintf(f,"__________________________________________________________________________\n\n\n\n");
@@ -82,7 +85,7 @@ run(int sf, double scale, double _5ht, int r_delay ,int r_life)
 				if(c2==0)
 				{	
 					reward_sp = true;
-					printf("-------------------reward has spawned!-------------------\n");
+			//		printf("-------------------reward has spawned!-------------------\n");
 					fprintf(f,"-------------------reward has spawned!-------------------\n");
 					x=2;
 				}
@@ -90,26 +93,26 @@ run(int sf, double scale, double _5ht, int r_delay ,int r_life)
 				if(reward_sp==true && c2>=r_life)
 				{
 					reward_sp = false;
-					printf("-------------------reward has despawned!-------------------\n");
+			//		printf("-------------------reward has despawned!-------------------\n");
 					fprintf(f,"-------------------reward has despawned!-------------------\n");
 				} 
 			}
-			printf("step : %i, position: %lf, speed:, %lf, 5ht: %g, vis: %g\n",steps ,pos, speed_out, _5ht, x);
+		//	printf("step : %i, position: %lf, speed:, %lf, 5ht: %g, vis: %g\n",steps ,pos, speed_out, _5ht, x);
 		fprintf(f,"step : %i, position: %lf, speed:, %lf, 5ht: %g, vis: %g\n",steps ,pos, speed_out, _5ht, x);
 		}
 		else
 		{
 				
 				fprintf(f,"\n\n\n ...done\n");
-				printf("\n\n\n ...done\n");
+			//	printf("\n\n\n ...done\n");
 				if(reward_sp)
 				{
-					printf("reward was successfully collected!\n\n");
+			//		printf("reward was successfully collected!\n\n");
 					fprintf(f,"reward was successfully collected!\n\n");
 				}
 				else
 				{
-					printf("reward was not successfully collected!\n\n");
+			//		printf("reward was not successfully collected!\n\n");
 					fprintf(f,"reward was not successfully collected!\n\n");
 				}
 				end = true;
@@ -126,10 +129,18 @@ run(int sf, double scale, double _5ht, int r_delay ,int r_life)
 }
 
 int 
-fileOut(char* name)
+fileOut()
 {
-	strtok(name, "\n");
-	strcat(name,".txt");
+    
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    
+    char name[sizeof("log%d-%d-%d %d:%d:%d.txt")] = "log";
+    char* date = malloc(sizeof("%d-%d-%d %d:%d:%d"));
+    
+    sprintf(date, "%d-%d-%d %d:%d:%d",tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	strcat(name, date);
+    strcat(name, ".txt");
 	f = fopen(name, "w");
 	return 0;
 }
@@ -158,82 +169,70 @@ quit(char* str)
 int
 main()
 {
+    
+    char fname_in[256];
+    char scale_in[64];
+    char sp_delay_in[64];
+    char reward_life_in[64];
+    char _5ht_in[4];
+    
+    double _5ht;
 
 	bool prog = true;
-	char scale_in[16];
-	char _5ht_in[16];
-	char sf_in[16];
-	char rs_in[16];
-	char rl_in[16];
-	char fname_in[256];
+
+    
+    fileOut();
 
 
-	bool first = true;
 
 	printf("Welcome to Alex's mPFC activity measurement prototype! Type \"quit\" to quit.\n\n");	
 	
 
-	while(true)
-	{
-
-		if(first)
-		{
-
-			printf("Enter a name for the output file\n\n>");
-			fgets(fname_in,256,stdin);
-			if(quit(fname_in)==1)return 0;
-
-			fileOut(fname_in);
-			first=false;
-		}
-
-		printf("Enter the size of the movement scale\n\n>");
-		fgets(scale_in,256,stdin);
+	//while(true)
+	//{
 
 
-		if(quit(scale_in)==1)return 0;
-	
+		printf("Enter 5ht value or \"quit\" to quit\n\n>");
+        fgets(_5ht_in,256,stdin);
+        if(quit(_5ht_in)==1)return 0;
+        
+         _5ht = atof(_5ht_in);
 		
-		printf("Enter the max 5ht value\n\n>");
-		fgets(_5ht_in,256,stdin);		
-
-		if(quit(_5ht_in)==1)return 0;
-
-		while(prog)
-		{
-
-			printf("Enter a speed scaling factor (Must be a factor/multiple of 10 and > 0)\n\n>");
-
-			fgets(sf_in, 256, stdin);
-			if(quit(sf_in)==1)return 0;
-			
-			if((atof(sf_in) != 1 && fmod(atof(sf_in), 10) != 0)|| atof(sf_in)==0 || atof(sf_in)<0)
-			{
-				printf("\nInvalid input.\n\n");
-			}
-			
-			else
-			{
-				prog = false;
-			}
-
-			
-
-			
-		}
-
-		printf("Enter a reward spawning delay. \n\n>");
-		fgets(rs_in,256,stdin);
-
-		if(quit(rs_in)==1)return 0;
-
-		printf("Enter a reward lifetime\n\n>");
-		fgets(rl_in,256,stdin);
-
-		if(quit(rl_in)==1)return 1;
-
-		run(atoi(sf_in),atof(scale_in), atof(_5ht_in), atoi(rs_in), atoi(rl_in));
-
+		printf("Enter max scale length, or type \"quit\" to quit\n\n>");
+        fgets(scale_in,256,stdin);
+        if(quit(scale_in)==1)return 0;
+        
+        
+        printf("Enter a max reward spawning delay, or type \"quit\" to quit\n\n>");
+        fgets(sp_delay_in, 256, stdin);
+        if(quit(sp_delay_in)==1)return 0;
+        
+        printf("Enter a max reward lifetime, or type \"quit\" to quit \n\n>");
+        
+        fgets(reward_life_in, 256, stdin);
+        if(quit(reward_life_in)==1)return 0;
+        
+        printf("calculating scaling...");
+        
+        for(int sl = 10; sl<=atoi(scale_in); sl=sl+10 ) //for scale lengths
+        {
+            for(int rs = 1; rs<=atoi(sp_delay_in); rs++) //for 
+            {
+                for(int rd = 1; rd<=atoi(reward_life_in); rd++)
+                {
+                        run(10, (double)sl, (double)_5ht, (double)rs, (double)rd);
+                }
+            }
+        }
+        
+        printf("done\n\n");
+        //for scale length (10s)
+        //for rs
+        //for rl
+        
+//  run(int sf, double scale, double _5ht, int r_delay ,int r_life)
+      
+    
 
 
 
@@ -242,7 +241,7 @@ main()
 		
 		
 		
-	}
+	//}
 
 
 
